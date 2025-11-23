7647ccddbcf4f55949a2889fdb67b4d8cd5e9ff0
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2025-11-23 02:09:27 +0200

    initial commit

---
+++ src/main/java/com/example/keyandgo/KeyAndGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyAndGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyAndGoApplication.class, args);
+    }
+
+}
