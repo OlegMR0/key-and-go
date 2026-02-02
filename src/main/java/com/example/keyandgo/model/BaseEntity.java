9ee6f717c192177647a42d0fde116d14264de6c8
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2026-02-02 18:06:59 +0200

    minor implementations

--- src/main/java/com/example/keyandgo/model/BaseEntity.java
+++ src/main/java/com/example/keyandgo/model/BaseEntity.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @MappedSuperclass
+@SQLRestriction("is_deleted = false")
 abstract class BaseEntity {
 
     @Id
@@ -21,6 +24,9 @@ abstract class BaseEntity {
 
     @Column(nullable = false)
     private LocalDateTime lastUpdatedAt;
+
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
 
     @PrePersist
     protected void onCreate() {
--- src/main/java/com/example/keyandgo/model/User.java
+++ src/main/java/com/example/keyandgo/model/User.java
@@ -1,22 +1,25 @@
 package com.example.keyandgo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
 
 @Data
+@SQLDelete(sql = "UPDATE ? SET is_deleted = true WHERE id = ?")
+@Table(name = "users")
 @Entity
-public class User {
+public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(nullable = false)
+    private String firstName;
 
-    private String username;
+    @Column(nullable = false)
+    private String lastName;
 
+    @Column(nullable = false, length = 60)
     private String password;
 
+    @Column(nullable = false, unique = true)
     private String email;
+
 }
